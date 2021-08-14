b83adc7fd5c0575495ab760de7a34cb331c42529
Vadim Nevyglas <>
2021-08-14 21:47:43 +0300

    Init commit

---
+++ JSON Handler/JSON Handler/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  JSON Handler
+//
+//  Created by Vadim Nevyglas on 14.08.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
